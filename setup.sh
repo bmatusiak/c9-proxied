d2121adfc7e682f8e7f51e6a19d95c83fde170a4
Bradley Matusiak <bmatusiak@gmail.com>
2013-10-28 21:20:53 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,10 @@
-
-#
-# wget http://nodejs.org/dist/v0.8.26/node-v0.8.26-linux-x86.tar.gz
-# tar -xvf node-v0.8.26-linux-x86.tar.gz
-# mv node-v0.8.26-linux-x86 node-package
-# sudo mv node-package/bin/node /usr/local/bin/node
-# sudo mv node-package/lib/node_modules  /usr/local/lib/node_modules
-# sudo ln -s /usr/local/lib/node_modules/npm/bin/npm-cli.js /usr/local/bin/npm
-# rm -rf  node-v* node-package
-#
-
-
-
+wget http://nodejs.org/dist/v0.8.26/node-v0.8.26-linux-x86.tar.gz
+tar -xvf node-v0.8.26-linux-x86.tar.gz
+mv node-v0.8.26-linux-x86 node-package
+sudo mv node-package/bin/node /usr/local/bin/node
+sudo mv node-package/lib/node_modules  /usr/local/lib/node_modules
+sudo ln -s /usr/local/lib/node_modules/npm/bin/npm-cli.js /usr/local/bin/npm
+rm -rf  node-v* node-package
 
 git clone https://github.com/bmatusiak/c9-proxied.git $HOME/c9-proxy
 
