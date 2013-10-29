50dabccc1f800eb2090fa7884e529458069c3bee
Bradley Matusiak <bmatusiak@gmail.com>
2013-10-28 21:22:26 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#node.js install
 wget http://nodejs.org/dist/v0.8.26/node-v0.8.26-linux-x86.tar.gz
 tar -xvf node-v0.8.26-linux-x86.tar.gz
 mv node-v0.8.26-linux-x86 node-package
@@ -6,25 +7,22 @@ sudo mv node-package/lib/node_modules  /usr/local/lib/node_modules
 sudo ln -s /usr/local/lib/node_modules/npm/bin/npm-cli.js /usr/local/bin/npm
 rm -rf  node-v* node-package
 
+#proxy install
 git clone https://github.com/bmatusiak/c9-proxied.git $HOME/c9-proxy
-
-cd c9-proxy
+cd $HOME/c9-proxy
 npm install
-
 echo "adding start up script to /etc/rc.local "
 rclocalcontent=$(cat /etc/rc.local)
 echoLine=$(echo $HOME'/c9-proxy/run-proxy.sh &\n\n')
-
 echo $echoLine > ./rc.local
 cat /etc/rc.local >> ./rc.local
 sudo mv ./rc.local /etc/rc.local
 
-
-git clone https://github.com/bmatusiak/cloud9.git
-cd cloud9
+#cloud9 install
+git clone https://github.com/bmatusiak/cloud9.git $HOME/c9-proxy/cloud9
+cd $HOME/c9-proxy/cloud9
 git checkout c9-proxied
 npm install
 npm install mongoose pty.js
 
-
-cd ..
+cd $HOME
