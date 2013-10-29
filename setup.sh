13bc92a64b9851ad937ec37d1955f32c7b67b405
Bradley Matusiak <bmatusiak@gmail.com>
2013-10-28 20:38:32 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@ npm install
 
 echo "adding start up script to /etc/rc.local "
 rclocalcontent=$(cat /etc/rc.local)
-sudo su - root -c 'echo -en "'$HOME'/c9-proxy/run-proxy.sh &\n\n'$rclocalcontent'" > /etc/rc.local'
+echoLine='echo -en "'$HOME'/c9-proxy/run-proxy.sh &\n\n'$rclocalcontent'" > /etc/rc.local'
+sudo su - root -c $($echoLine)
 
 git clone https://github.com/bmatusiak/cloud9.git
 cd cloud9
