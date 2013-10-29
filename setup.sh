a1c837f7d5d833ce899872b8bdcea3887c90aa55
Bradley Matusiak <bmatusiak@debian>
2013-10-28 20:27:10 -0400

    setup.sh exit

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
-git clone https://github.com/bmatusiak/c9-proxied.git ./c9-proxy
+git clone https://github.com/bmatusiak/c9-proxied.git $HOME/c9-proxy
 
 cd c9-proxy
 npm install
+
+echo "adding start up script to /etc/rc.local "
+rclocalcontent=$(cat /etc/rc.local)
+sudo su - root -c 'echo -en "$HOME/c9-proxy/run-proxy.sh &\n\n$rclocalcontent" > /etc/rc.local'
 
 git clone https://github.com/bmatusiak/cloud9.git
 cd cloud9
 git checkout c9-proxied
 npm install
+npm install mongoose pty.js
+
 
 cd ..
-content=$(cat /etc/rc.local) # no cat abuse this time
-
-sudo su - root -C 'echo -en "$HOME/c9-proxy/run-proxy.sh &\n\n$content" > /etc/rc.local'
